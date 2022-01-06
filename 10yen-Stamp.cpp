79a556c31f6b97fce968e72a3786db7acaf6d7b0
ngzhekai <ngzhekai@gmail.com>
2022-01-06 23:21:55 +0800

    edited 10yen Stamps.cpp

--- 10yen-Stamp.cpp
+++ 10yen-Stamp.cpp
@@ -1,3 +1,16 @@
+/* 
+Links to the task hosted on atcoder.jp https://atcoder.jp/contests/abc233/tasks/abc233_a
+
+Problem Statement
+Takahashi wants to send a letter to Santa Claus. He has an envelope with an X-yen (Japanese currency) stamp stuck on it.
+To be delivered to Santa Claus, the envelope must have stamps in a total value of at least Y yen.
+Takahashi will put some more 10-yen stamps so that the envelope will have stamps worth at least Y yen in total.
+At least how many more 10-yen stamps does Takahashi need to put on the envelope?
+
+Constraints
+X and Y are integers.
+1≤X,Y≤1000
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
