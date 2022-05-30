f50e77e7de873cbd15550bb54f2e3b411143e8f7
ngzhekai <ngzhekai@gmail.com>
2022-05-30 23:25:03 +0800

    added solution for ASCII_code.cpp

---
+++ ASCII-code.cpp
@@ -0,0 +1,23 @@
+/* 
+Link to the task hosted on atcoder.jp https://atcoder.jp/contests/abc252/tasks/abc252_a
+
+Problem Statement
+The ASCII values of the lowercase English letters a, b, …, z are 97,98,…,122 in this order.
+
+Given an integer N between 97 and 122, print the letter whose ASCII value is N.
+
+Constraints
+N is an integer between 97 and 122 (inclusive).
+
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    cout << char(n);
+    return 0;
+}
